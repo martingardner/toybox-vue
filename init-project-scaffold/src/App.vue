c63815b58e61b2fbfcb9365170e8717bc73b616e
Martin Gardner II <martingardnerii@gmail.com>
2024-02-15 19:58:00 -0600

    init project setup

---
+++ init-project-scaffold/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { ref } from 'vue'
+  import ObjectCount from './components/ObjectCount.vue';
+  const count = ref(0)
+</script>
+
+
+<template>
+  <main>
+    <button @click="count++">Count is: {{ count }}</button>
+    <ObjectCount />
+  </main>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold
+}
+</style>
